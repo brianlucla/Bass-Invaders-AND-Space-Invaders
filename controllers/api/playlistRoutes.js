6a628b2f6db289275533736d8bf8ac2f3e7a4699
brianlu18 <brianlu18@g.ucla.edu>
2023-07-12 14:58:56 -0500

    changes to routes and public js

--- controllers/api/playlistRoutes.js
+++ controllers/api/playlistRoutes.js
@@ -1,14 +1,32 @@
 const router = require("express").Router();
 const { Playlist, Song } = require("../../models");
 const withAuth = require('../../helpers/auth');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-router.post("/", withAuth, async (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const playlistData = await Playlist.findByPk(req.params.id);
+    res.json(playlistData);
+  } catch(error){
+    res.status(500).json(error);
+  }
+})
+
+router.post("/", async (req, res) => {
   // create playlist
   try {
+    const songArray = await giveSongs(req.body.inputVal);
+    console.log(songArray);
     const playlistData = await Playlist.create({
       userId: req.session.userId,
+      song_array: songArray,
     });
     //response
+    console.log(playlistData.getDataValue(song_array));
     res.json(playlistData);
   } catch (error) {
     res.status(500).json(error);
@@ -46,36 +64,39 @@ router.delete("/", withAuth, async (req, res) => {
   }
 });
 
-// router.get("/favorites", async (req, res) => {
-//   // find all
-//   try {
-//     const favoritesData = await Playlist.findAll({
-//       raw: true,
-//       where: { favorite: true },
-//       include: [Song],
-//     });
-//     const favorites = favoritesData.map((favorite) => {
-//         favorite.get({plain:true});
-//     });
-//     res.render('favorites', { favorites });
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
+const giveSongs = async function (input) {
+  const gptResponse = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Give me a list of 10 songs to listen to when I feel ${input}`,
+    max_tokens: 100,
+    temperature: 0.9,
+  });
 
-// router.get("/history", async (req, res) => {
-//   try {
-//     const playlistData = await Playlist.findAll({
-//       raw: true,
-//       include: [Song],
-//     });
-//     const songs = playlistData.map((song) =>{
-//         song.get({plain:true});
-//     });
-//     res.render('history', { songs });
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
+  const stringToSplit = gptResponse.data.choices[0].text;
+  const splitString = stringToSplit.split("\n");
+  const slicedArray = splitString.slice(2, 7);
+  const processedArray = [];
+  const slicedArraySplit = [];
+  for (let i = 0; i < slicedArray.length; i++) {
+    slicedArraySplit.push(slicedArray[i].split("."));
+  }
+
+  for (let i = 0; i < slicedArraySplit.length; i++) {
+    const songArtist = slicedArraySplit[i][1].split("by");
+    const songTitle = songArtist[0].replace(/['"]+/g, "").slice(1, -1);
+    const artistName = songArtist[1].slice(1);
+    // const youtubeURL = generateYoutubeURL(songTitle, artistName);
+    const songObject = {
+      songName: songTitle,
+      artistName: artistName,
+      // youtube_url: youtubeURL,
+    };
+    processedArray.push(songObject);
+  }
+  console.log("yay");
+  return processedArray;
+};
+
+
 
 module.exports = router;
--- models/Playlist.js
+++ models/Playlist.js
@@ -7,24 +7,34 @@ class Playlist extends Model {}
 
 Playlist.init(
   {
-    id:{
-      type:DataTypes.INTEGER,
-      primaryKey:true,
-      autoIncrement:true,
-      allowNull:false,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    favorite:{
+    favorite: {
       type: DataTypes.BOOLEAN,
-      allowNull:false,
-      defaultValue:false,
+      allowNull: false,
+      defaultValue: false,
+    },
+    song_array: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      get() {
+        return this.getDataValue("song_array").split(";");
+      },
+      set(val) {
+        this.setDataValue("song_array", val.join(";"));
+      },
     },
   },
   {
     sequelize,
     timestamps: false,
-    modelName: 'playlist',
-    freezeTableName:true,
+    modelName: "playlist",
+    freezeTableName: true,
   }
-  );
+);
 
 module.exports = Playlist;
