3367fc9ad56be5cdbc127f6e094906e1022e557a
brianlu18 <brianlu18@g.ucla.edu>
2023-07-12 14:58:56 -0500

    changes to routes and public js

--- controllers/api/playlistRoutes.js
+++ controllers/api/playlistRoutes.js
@@ -6,7 +6,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const baseApiUrlY = "https://www.googleapis.com/youtube/v3";
 
 router.get('/:id', async (req, res) => {
   try {
@@ -20,8 +19,11 @@ router.get('/:id', async (req, res) => {
 router.post("/", async (req, res) => {
   // create playlist
   try {
+    const songArray = await giveSongs(req.body.inputVal);
+    console.log(songArray);
     const playlistData = await Playlist.create({
       userId: req.session.userId,
+      song_array: songArray,
     });
 
     const songArray = await giveSongs(req.body.inputVal, playlistData.id);
@@ -30,7 +32,8 @@ router.post("/", async (req, res) => {
     const songData = Song.bulkCreate(songArray);
     
     //response
-    res.render('');
+    console.log(playlistData.getDataValue(song_array));
+    res.json(playlistData);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -67,7 +70,7 @@ router.delete("/", withAuth, async (req, res) => {
   }
 });
 
-const giveSongs = async function (input, id) {
+const giveSongs = async function (input) {
   const gptResponse = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `Give me a list of 10 songs to listen to when I feel ${input}`,
@@ -88,76 +91,16 @@ const giveSongs = async function (input, id) {
     const songArtist = slicedArraySplit[i][1].split("by");
     const songTitle = songArtist[0].replace(/['"]+/g, "").slice(1, -1);
     const artistName = songArtist[1].slice(1);
-    const songSearchTerms = songTitle.split(" ");
-    const artistSearchTerms = artistName.split(" ");
-    let combinedSearchTerm = "";
-
-    for (let i = 0; i < songSearchTerms.length; i++) {
-      combinedSearchTerm += `${songSearchTerms[i]}+`;
-    }
-
-    for (let j = 0; j < artistSearchTerms.length; j++) {
-      if (j === artistSearchTerms.length - 1) {
-        combinedSearchTerm += `${artistSearchTerms[j]}`;
-      } else {
-        combinedSearchTerm += `${artistSearchTerms[j]}+`;
-      }
-    }
-
-    const apiURL = `${baseApiUrlY}/search?key=${process.env.YOUTUBE_KEY}&part=snippet&q=${combinedSearchTerm}&maxResults=1`;
-
-    console.log(apiURL);
-
-    const youtubeURL = await fetch(apiURL)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.items[0].id.videoId);
-        console.log(
-          `https://www.youtube.com/embed/${data.items[0].id.videoId}`
-        );
-        return `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-      });
+    // const youtubeURL = generateYoutubeURL(songTitle, artistName);
     const songObject = {
-      song_title: songTitle,
-      artist: artistName,
-      youtube_url: youtubeURL,
-      playlist_id:id
+      songName: songTitle,
+      artistName: artistName,
+      // youtube_url: youtubeURL,
     };
     processedArray.push(songObject);
-    
   }
-  
+  console.log("yay");
   return processedArray;
-};
-
-const generateYoutubeURL = function (songName, artistName) {
-  const songSearchTerms = songName.split(" ");
-  const artistSearchTerms = artistName.split(" ");
-  let combinedSearchTerm = "";
-
-  for (let i = 0; i < songSearchTerms.length; i++) {
-    combinedSearchTerm += `${songSearchTerms[i]}+`;
-  }
-
-  for (let j = 0; j < artistSearchTerms.length; j++) {
-    if (j === artistSearchTerms.length - 1) {
-      combinedSearchTerm += `${artistSearchTerms[j]}`;
-    } else {
-      combinedSearchTerm += `${artistSearchTerms[j]}+`;
-    }
-  }
-
-  const apiURL = `${baseApiUrlY}/search?key=${process.env.YOUTUBE_KEY}&part=snippet&q=${combinedSearchTerm}&maxResults=1`;
-
-  console.log(apiURL);
-
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.items[0].id.videoId);
-      console.log(`https://www.youtube.com/embed/${data.items[0].id.videoId}`);
-      return `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-    });
 };
 
 
