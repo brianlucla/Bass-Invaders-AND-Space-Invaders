ceb87b4d51253a6a68d03b79e7556d278256fc88
brianlu18 <brianlu18@g.ucla.edu>
2023-07-12 17:47:01 -0500

    update favorites working

--- public/js/playlistGenerator.js
+++ public/js/playlistGenerator.js
@@ -5,22 +5,27 @@
 const inputEl = document.getElementById('search-song');
 const submitEl = document.getElementById('submit-song');
 const playlistEl = document.getElementById('single-playlist');
+const faves = document.getElementById("favorite-btn");
 const video1El = document.getElementById('video-1');
 const video2El = document.getElementById('video-2');
 const video3El = document.getElementById('video-3');
 const video4El = document.getElementById('video-4');
 const video5El = document.getElementById('video-5');
 
+let playlistID;
+
+
 
 const songHandler = async function (event) {
   event.preventDefault();
 
   // Create playlist/songs and add to songs to playlist
-  // const playlistSongCreator = await playlistCreate();
-  // console.log("this is what we're looking for",playlistSongCreator.id);
+  const playlistSongCreator = await playlistCreate();
+  console.log("this is what we're looking for",playlistSongCreator.id);
+  playlistID = playlistSongCreator.id
 
   // render
-  const getSong = await fetch(`/api/playlist/20`,{
+  const getSong = await fetch(`/api/playlist/${playlistID}`,{
     method:'GET',
     headers:{
       'Content-Type':'application/json'
@@ -29,6 +34,7 @@ const songHandler = async function (event) {
 
   let i = 1;
   const songData = await getSong.json();
+  playlistEl.textContent = '';
   const mappedArray = songData.songs.map((song)=>{
     playlistEl.textContent += `\n
     Song Title: ${song.song_title}\n
@@ -43,10 +49,6 @@ const songHandler = async function (event) {
     
     
   });
-
-
-  
-
 };
 
 const playlistCreate = async function(){
@@ -63,7 +65,25 @@ const playlistCreate = async function(){
   return playlistResponse.json();
 }
 
-submitEl.addEventListener('click', songHandler);``
+const favoritePlaylist = async function(){
+  
+  console.log(playlistID);
+  const response = await fetch(`/api/playlist/${playlistID}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    console.log("Item favorite status updated");
+    // Perform any necessary actions after updating the favorite status
+  } else {
+    console.log("Failed to update item favorite status");
+  }
+}
+
+submitEl.addEventListener('click', songHandler);
 
 
-
+faves.addEventListener("click", favoritePlaylist);
